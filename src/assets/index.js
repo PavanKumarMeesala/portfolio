aa3e196480061ffd24fadeda146e757cf16ceb96
PavanKumarMeesala <pavankumarmeesala43@gmail.com>
2025-05-25 19:29:40 +0530

    Error close

--- src/assets/index.js
+++ src/assets/index.js
@@ -15,7 +15,6 @@ import css from "./tech/css.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import tailwind from "./tech/tailwind.png";
 import spring from "./tech/spring.png";
